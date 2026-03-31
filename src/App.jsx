e3447b7a286c6173dea93c16151d02008c4cb106
nisarfatima166-coder <nisarfatima166@gmail.com>
2026-03-30 22:40:15 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+export default function App (){
+  return (
+    <>
+    <div class="share-container">
+
+   
+    <input type="checkbox" id="toggle-share"/>
+
+    
+    <label class="share-btn" for="toggle-share"></label>
+
+
+    <div class="social-icons">
+        <a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="twitter"><i class="fab fa-twitter"></i></a>
+        <a href="#" class="instagram"><i class="fab fa-instagram"></i></a>
+        <a href="#" class="youtube"><i class="fab fa-youtube"></i></a>
+    </div>
+
+</div>
+
+    
+    </>
+  )
+}
